b46fbe2bd05ab42c4fd7a9536966a25e15001a3f
Todd Bednarczyk <echo@dronda.org>
2022-11-30 21:33:56 -0500

    initial commit

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,16 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Thread.currentThread().contextClassLoader.getResourceAsStream(name)
+    .reader()
+    .readLines()
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
