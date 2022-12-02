2240814b8822f2f478aaf146a631db78a6d8268b
Todd Bednarczyk <echo@dronda.org>
2022-12-01 21:24:29 -0500

    add template

---
+++ src/main/kotlin/DayXX.kt
@@ -0,0 +1,18 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    println(part1(testInput))
+
+    val input = readInput("Day01")
+//    println(part1(input))
+//    println(part2(input))
+}
