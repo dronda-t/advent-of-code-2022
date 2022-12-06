562726f1c25c7c958766097e6a67d87eb7310797
Todd Bednarczyk <echo@dronda.org>
2022-12-06 11:59:47 -0500

    Day06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,24 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        input.first().windowed(4).forEachIndexed { index, s ->
+            if (s.toHashSet().size == 4) return index + 4
+        }
+        error("")
+    }
+
+    fun part2(input: List<String>): Int {
+        input.first().windowed(14).forEachIndexed { index, s ->
+            if (s.toHashSet().size == 14) return index + 14
+        }
+        error("")
+    }
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day06_test")
+//    println(part2(testInput))
+
+    val input = readInput("Day06")
+//    println(part1(input))
+    println(part2(input))
+}
